29dc46ecefb5592d12a8d927ea386d32535d4622
knyaleks <knyaleks@yandex.ru>
2022-10-21 11:36:21 +0300

    task 10, 13, 15

---
+++ task_13/Program.cs
@@ -0,0 +1,14 @@
+﻿//Задача 13: Программа, которая выводит третью цифру заданного числа 
+//или сообщает, что третьей цифры нет.
+
+Console.Write ("Введите целое число ");
+//убираем знак минус у отрицательного числа  
+int number= Math.Abs(Convert.ToInt32(Console.ReadLine()));  
+string numberStr=Convert.ToString(number); //преобразуем число в строку
+if (numberStr.Length < 3) 
+    Console.WriteLine("Третьей цифры нет");
+else  
+    Console.Write("Третья цифра введенного числа  " + numberStr[2]);
+   
+ 
+
