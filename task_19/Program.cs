baef57dd68ae9cfd6dc6ae93bb37e8edb6625752
knyaleks <knyaleks@yandex.ru>
2022-10-25 02:28:29 +0300

    The third practical task

---
+++ task_19/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 19
+// Программа, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет    12821 -> да   23432 -> да
+Console.Write ("Введите целое число ");  
+int number=Convert.ToInt32(Console.ReadLine());
+// Вычисляем кол-во цифр в числе
+int numDigits= Convert.ToInt32(Math.Floor(Math.Log10(( Math.Abs(number)))))+1;
+// Определяем степени числа десять для вычисления первой (левой) цифры числа и последней (правой) цифры
+int degreeLeft=numDigits-1, degreeRight=0; 
+// Определяем кол-во итераций в цикле  n (половина введенного числа)
+int i=0, n=numDigits/2;
+while (i<n)
+{
+if (Math.Floor((number/(Math.Pow(10,degreeLeft)))%10)
+ == Math.Floor((number/(Math.Pow(10,degreeRight)))%10)) 
+{
+   degreeLeft=degreeLeft-1;
+   degreeRight=degreeRight+1;
+   i=i+1;
+}
+else
+{
+   break;
+}
+}
+if (i==n)
+    Console.Write ("Введенное число - палиндром");   
+else
+    Console.Write ("Введенное число - не палиндром");    
+ 
+ 
+   
+  
---
+++ task_23/Program.cs
@@ -0,0 +1,8 @@
+﻿// Задача 23
+//Программа, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+Console.WriteLine ("Введите целое положительное число"); 
+int N=Convert.ToInt32(Console.ReadLine());
+for (int i=1; i<=N; ++i)
+{
+Console.Write($"{Math.Pow(i,3)} ");
+}
