4337ce5f3328c36cc05d06f9c4b9bb8e7993e33e
knyaleks <knyaleks@yandex.ru>
2022-11-20 00:27:07 +0300

    The six practical task

---
+++ task_41/Program.cs
@@ -0,0 +1,28 @@
+﻿/* Задача 41: Пользователь вводит с клавиатуры M чисел.
+Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2    1, -7, 567, 89, 223-> 4  */
+
+Console.WriteLine("Введите цифры через пробел. Нажатие <Enter> означает окончание ввода");
+string elements = Console.ReadLine();
+int[] numArray = GetArrayFromString(elements);
+Console.WriteLine($"[{String.Join(", ", numArray)}]");
+
+int j = 0;
+for (int i = 0; i < numArray.Length; ++i)
+{
+  if (numArray[i] > 0) ++j;
+}
+
+Console.WriteLine($"Введено {j} числа больше нуля");
+
+// Функция создания массива из строки, содержащей цифры
+int[] GetArrayFromString(string Array)
+{
+  string[] nums = Array.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+  int[] res = new int[nums.Length];
+  for (int i = 0; i < nums.Length; i++)
+  {
+    res[i] = int.Parse(nums[i]);
+  }
+  return res;
+}
---
+++ task_43/Program.cs
@@ -0,0 +1,30 @@
+﻿/*  Задача 43: Написать программу, которая найдёт точку пересечения двух прямых, 
+заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+значения b1, k1, b2 и k2 задаются пользователем.
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Решение:
+В точке пересечения у обеих прямых совпадают кординаты x и y.
+Из рав-ва координат y следует, что  k1 * x + b1 = k2 * x + b2.
+Переносом и перестановкой получаем x = (b2-b1) / (k1-k2)    */  
+
+   
+Console.Write ("Введите b1  ");
+int b1 = Convert.ToInt32(Console.ReadLine());
+Console.Write ("Введите k1  ");
+int k1 = Convert.ToInt32(Console.ReadLine());
+Console.Write ("Введите b2  ");
+int b2 = Convert.ToInt32(Console.ReadLine());
+Console.Write ("Введите k2  ");
+int k2 = Convert.ToInt32(Console.ReadLine());
+double x = (double)(b2 - b1) / (k1 - k2);
+double y = k1 * x + b1;
+Console.WriteLine ("Координаты точки пересечения двух прямых");
+Console.WriteLine ($"x = {x} , y = {y}");
+  
+ 
+ 
+
+
+
+
