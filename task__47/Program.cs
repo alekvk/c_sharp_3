9ada1875bfa360c06a6744b9de9112c481c05945
knyaleks <knyaleks@yandex.ru>
2022-11-20 15:37:51 +0300

    The seventh practical task

---
+++ task__47/Program.cs
@@ -0,0 +1,52 @@
+﻿/* Задача 47. Задайте двумерный массив размером m * n, 
+заполненный случайными вещественными числами.
+m = 3, n = 4.
+0,5 7 -2 -0,2
+1 -3,3 8 -9,9
+8 7,8 -7,1 9        */
+
+Console.Write ("Введите количество строк в массиве  ");
+uint r = Convert.ToUInt32(Console.ReadLine());
+Console.Write ("Введите количество столбцов в массиве  ");
+uint c = Convert.ToUInt32(Console.ReadLine());
+Console.Write ("Введите нижнюю границу дипазона случайных чисел  ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write ("Введите верхнюю границу дипазона случайных чисел   ");
+int max = Convert.ToInt32(Console.ReadLine());
+double [,] arr = CreateTwoDimensArray (r, c, min, max);
+PrintArray(arr);
+
+//  Функция вывода на экран элементов двуразмерного массива
+void PrintArray (double [,] array)
+{
+    Console.WriteLine ("Элементы массива :");
+    for (int i = 0; i < array.GetLength(0); i++)
+	{
+		for (int j = 0; j < array.GetLength(1); j++)
+		{
+			Console.Write ($"{array[i,j]}     ");
+		}
+	Console.WriteLine();
+	}
+}
+
+// Функция создания  двумерного массива случайных вещественных чисел
+double [,] CreateTwoDimensArray (uint row, uint col, int minValue, int maxValue)
+{
+  double [,] array = new double [row, col];
+  for (int i = 0; i < row; i++)
+  { 
+    for (int j = 0; j < col; j++)
+    {
+	    double n = new Random().NextDouble();
+        double m = n * (new Random().Next(minValue, maxValue + 1)) ;
+        array[i,j] =  Math.Round(m, 1);
+	}
+  }
+  return array;
+}  
+        		
+     
+	 
+	 
+	 
---
+++ task__50/Program.cs
@@ -0,0 +1,50 @@
+﻿/* Задача 50. Написать программу, которая на вход принимает позиции элемента в двумерном массиве, 
+и возвращает значение этого элемента или же указание, что такого элемента нет.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+17 -> такого числа в массиве нет */
+
+// Задаем массив целых чисел
+int [,] matrix = new int[,]
+{
+	{45, 56, 5, 9, 78, 32},
+	{785, 5, 66, 93, 85, 854},
+	{4, 56, 78, 85, 2, 52}
+};	
+	
+Console.WriteLine ("Укажите позицию элемента в массиве   ");
+Console.Write ("Укажите номер строки  ");
+uint numRow = Convert.ToUInt32(Console.ReadLine());
+Console.Write ("Укажите номер столбца  ");
+uint numCol = Convert.ToUInt32(Console.ReadLine());
+PrintArray(matrix);
+if (numRow < matrix.GetLength(0) && numCol < matrix.GetLength(1))
+{
+   Console.Write($"Искомый элемент {matrix [numRow,numCol]}");
+}
+else
+{
+   Console.WriteLine("Такого элемента в массиве нет");
+} 
+
+// Функция вывода на экран элементов двуразмерного массива
+void PrintArray (int [,] array)
+{
+Console.WriteLine ("Элементы массива:");
+	for (int i = 0; i < array.GetLength(0); i++)
+	{
+		for (int j = 0; j < array.GetLength(1); j++)
+		{
+			Console.Write ($"{array[i,j]}    ");
+		}
+	Console.WriteLine();
+	}
+Console.WriteLine();    
+}
+
+
+	 
+	 
+	 
